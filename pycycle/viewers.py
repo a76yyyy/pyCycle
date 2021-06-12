656a14fc89cd397fd00913c0ac7465a5f7e5cb95
TDS98 <thibault.desmedt@me.com>
2021-06-12 17:39:51 +0200

    Improved result printing

--- pycycle/viewers.py
+++ pycycle/viewers.py
@@ -7,23 +7,25 @@ import matplotlib.pyplot as plt
 
 def print_flow_station(prob, fs_names, file=sys.stdout):
     names = ['tot:P', 'tot:T', 'tot:h', 'tot:S', 'stat:P', 'stat:W', 'stat:MN', 'stat:V', 'stat:area']
+    units = ['[lbf/inch2]', '[degR]', '[Btu/lbm]', '[Btu/lbm/degR]', '[lbf/inch2]', '[lbm/s]', '[-]', '[ft/s]', '[inch2]']
 
     n_names = len(names)
-    line_tmpl = '{:<23}|  '+'{:>13}'*n_names
-    len_header = 27+13*n_names
+    line_tmpl = '{:<27}|  '+'{:>17}'*n_names
+    len_header = 30+17*n_names
 
     print("-"*len_header, file=file, flush=True)
-    print("                            FLOW STATIONS", file=file, flush=True)
+    print("                                                                                    FLOW STATIONS", file=file, flush=True)
     print("-"*len_header, file=file, flush=True)
 
     # header_line
     vals = ['Flow Station'] + names
+    unit = ['Units'] + units
     print('-'*len_header, file=file, flush=True)
     print(line_tmpl.format(*vals), file=file, flush=True)
+    print(line_tmpl.format(*unit), file=file, flush=True)
     print('-'*len_header, file=file, flush=True)
 
-
-    line_tmpl = '{:<23.23}|  ' + '{:13.3f}'*n_names
+    line_tmpl = '{:<27.27}|  ' + '{:17.3f}'*n_names
     for fs_name in fs_names:
         data = []
         for name in names:
@@ -37,27 +39,27 @@ def print_flow_station(prob, fs_names, file=sys.stdout):
 
 def print_compressor(prob, element_names, file=sys.stdout):
 
-    len_header = 17+12*13
-    # print("-"*len_header)
+    len_header = 17+12*12
     print("-"*len_header, file=file, flush=True)
-    print("                          COMPRESSOR PROPERTIES", file=file, flush=True)
+    print("                                                    COMPRESSOR PROPERTIES", file=file, flush=True)
     print("-"*len_header, file=file, flush=True)
 
-    line_tmpl = '{:<14}|  '+'{:>11}'*12
+    line_tmpl = '{:<20}|  '+'{:>11}'*12
     print(line_tmpl.format('Compressor', 'Wc', 'Pr', 'eta_a', 'eta_p', 'Nc', 'pwr', 'RlineMap', 'NcMap', 'PRmap', 'alphaMap', 'SMN', 'SMW'),
+          file=file, flush=True)
+    print(line_tmpl.format('Units', '[lbm/s]', '[-]', '[-]', '[-]', '[rpm]', '[hp]', '[-]', '[rpm]', '[-]', '[-]', '[-]', '[-]'),
           file=file, flush=True)
     print("-"*len_header, file=file, flush=True)
 
-
-    line_tmpl = '{:<14}|  '+'{:11.3f}'*12
+    line_tmpl = '{:<20}|  '+'{:11.3f}'*12
     for e_name in element_names:
         sys = prob.model._get_subsystem(e_name)
         if sys.options['design']:
-          PR_temp = prob[e_name+'.map.scalars.PR'][0]
-          eff_temp = prob[e_name+'.map.scalars.eff'][0]
+            PR_temp = prob[e_name+'.map.scalars.PR'][0]
+            eff_temp = prob[e_name+'.map.scalars.eff'][0]
         else:
-          PR_temp = prob[e_name+'.PR'][0]
-          eff_temp = prob[e_name+'.eff'][0]
+            PR_temp = prob[e_name+'.PR'][0]
+            eff_temp = prob[e_name+'.eff'][0]
 
         print(line_tmpl.format(e_name, prob[e_name+'.Wc'][0], PR_temp,
                                eff_temp, prob[e_name+'.eff_poly'][0], prob[e_name+'.Nc'][0], prob[e_name+'.power'][0],
@@ -70,13 +72,14 @@ def print_compressor(prob, element_names, file=sys.stdout):
 def print_burner(prob, element_names, file=sys.stdout):
     len_header = 23+4*13
     print("-"*len_header, file=file, flush=True)
-    print("                            BURNER PROPERTIES", file=file, flush=True)
+    print("                                                        BURNER PROPERTIES", file=file, flush=True)
     print("-"*len_header, file=file, flush=True)
 
     line_tmpl = '{:<20}|  '+'{:>13}'*4
     print(line_tmpl.format('Burner', 'dPqP', 'TtOut', 'Wfuel', 'FAR'), file=file, flush=True)
+    print(line_tmpl.format('Units', '[-]', '[degR]', '[lbm/s]', '[-]'), file=file, flush=True)
+    print("-" * len_header, file=file, flush=True)
 
-    # line_tmpl = '{:<20}|  '+'{:13.3f}'*4
     line_tmpl = '{:<20}|  {:13.4f}{:13.2f}{:13.4f}{:13.5f}'
     for e_name in element_names:
         W_fuel = prob[e_name+'.Wfuel'][0]
@@ -91,17 +94,19 @@ def print_burner(prob, element_names, file=sys.stdout):
 
 def print_turbine(prob, element_names, file=sys.stdout):
 
-    len_header = 17+7*13
+    len_header = 23+7*13
     print("-"*len_header, file=file, flush=True)
-    print("                            TURBINE PROPERTIES", file=file, flush=True)
+    print("                                                        TURBINE PROPERTIES", file=file, flush=True)
     print("-"*len_header, file=file, flush=True)
 
-    line_tmpl = '{:<14}|  '+'{:>13}'*7
+    line_tmpl = '{:<20}|  '+'{:>13}'*7
     print(line_tmpl.format('Turbine', 'Wp', 'PR', 'eff', 'Np', 'pwr', 'NpMap', 'PRmap'),
         file=file, flush=True)
+    print(line_tmpl.format('Units', '[lbm/s]', '[-]', '[-]', '[rpm]', '[hp]', '[rpm]', '[-]'),
+          file=file, flush=True)
+    print("-" * len_header, file=file, flush=True)
 
-
-    line_tmpl = '{:<14}|  '+'{:13.3f}'*7
+    line_tmpl = '{:<20}|  '+'{:13.3f}'*7
     for e_name in element_names:
         sys = prob.model._get_subsystem(e_name)
         if sys.options['design']:
@@ -119,26 +124,27 @@ def print_turbine(prob, element_names, file=sys.stdout):
 
 def print_nozzle(prob, element_names, file=sys.stdout):
 
-    len_header = 17+8*13
+    len_header = 23+8*13
     print("-"*len_header, file=file, flush=True)
-    print("                            NOZZLE PROPERTIES", file=file, flush=True)
+    print("                                                        NOZZLE PROPERTIES", file=file, flush=True)
     print("-"*len_header, file=file, flush=True)
 
-    line_tmpl = '{:<14}|  '+'{:>13}'*8
+    line_tmpl = '{:<20}|  '+'{:>13}'*8
     print(line_tmpl.format('Nozzle', 'PR', 'Cv', 'Cfg', 'Ath', 'MNth', 'MNout', 'V', 'Fg'), file=file, flush=True)
-
+    print(line_tmpl.format('Units', '[-]', '[-]', '[-]', '[inch2]', '[-]', '[-]', '[ft/s]', '[lbf]'), file=file, flush=True)
+    print("-" * len_header, file=file, flush=True)
 
     for e_name in element_names:
         sys = prob.model._get_subsystem(e_name)
         if sys.options['lossCoef'] == 'Cv':
             Cv_val = prob[e_name+'.Cv'][0]
             Cfg_val = '        N/A  '
-            line_tmpl = '{:<14}|  ' + '{:13.3f}'*2 + '{}' + '{:13.3f}'*5
+            line_tmpl = '{:<20}|  ' + '{:13.3f}'*2 + '{}' + '{:13.3f}'*5
 
         else:
             Cv_val = '        N/A  '
             Cfg_val = prob[e_name+'.Cfg'][0]
-            line_tmpl = '{:<14}|  ' + '{:13.3f}'*1 + '{}' + '{:13.3f}'*6
+            line_tmpl = '{:<20}|  ' + '{:13.3f}'*1 + '{}' + '{:13.3f}'*6
 
         print(line_tmpl.format(e_name, prob[e_name+'.PR'][0], Cv_val, Cfg_val,
                                prob[e_name+'.Throat:stat:area'][0], prob[e_name+'.Throat:stat:MN'][0],
@@ -161,12 +167,14 @@ def print_bleed(prob, element_names, file=sys.stdout):
 
     len_header = max_name_len+3+7*13
     print("-"*len_header, file=file, flush=True)
-    print("                            BLEED PROPERTIES", file=file, flush=True)
+    print("                                                        BLEED PROPERTIES", file=file, flush=True)
     print("-"*len_header, file=file, flush=True)
 
     max_name_len = str(max_name_len)
     line_tmpl = '{:<'+max_name_len+'}|  '+'{:>13}'*7
     print(line_tmpl.format('Bleed', 'Wb/Win', 'Pfrac', 'Workfrac', 'W', 'Tt', 'ht', 'Pt'), file=file, flush=True)
+    print(line_tmpl.format('Units', '[-]', '[-]', '[-]', '[lbm/s]', '[degR]', '[Btu/lbm]', '[lbf/inch2]'), file=file, flush=True)
+    print("-" * len_header, file=file, flush=True)
 
     line_tmpl = '{:<'+max_name_len+'}|  '+'{:13.3f}'*7
     for e_name in element_names:
@@ -194,18 +202,21 @@ def print_bleed(prob, element_names, file=sys.stdout):
                                    frac_work, prob[full_bleed_name+':stat:W'][0], prob[full_bleed_name+':tot:T'][0],
                                    prob[full_bleed_name+':tot:h'][0], prob[full_bleed_name+':tot:P'][0]),
                   file=file, flush=True)
+    print("-" * len_header, file=file, flush=True)
 
 
 def print_shaft(prob, element_names, file=sys.stdout):
 
-    len_header = len_header = 23+20*5
+    len_header = 23+20*5
 
     print("-"*len_header, file=file, flush=True)
-    print("                            SHAFT PROPERTIES", file=file, flush=True)
+    print("                                                        SHAFT PROPERTIES", file=file, flush=True)
     print("-"*len_header, file=file, flush=True)
 
     line_tmpl = '{:<20}|  '+'{:>20}'*5
     print(line_tmpl.format('Shaft', 'Nmech', 'trqin', 'trqout', 'pwrin', 'pwrout'), file=file)
+    print(line_tmpl.format('Units', '[rpm]', '[ft*lbf]', '[ft*lbf]', '[hp]', '[hp]'), file=file)
+    print("-" * len_header, file=file, flush=True)
 
     line_tmpl = '{:<20}|  '+'{:20.3f}'*5
     for e_name in element_names:
@@ -219,15 +230,18 @@ def print_shaft(prob, element_names, file=sys.stdout):
 
 def print_mixer(prob, element_names, file=sys.stdout):
 
-    len_header = len_header = 23+20*6
+    len_header = 23+20*6
 
     print("-"*len_header, file=file, flush=True)
-    print("                            MIXER PROPERTIES", file=file, flush=True)
+    print("                                                        MIXER PROPERTIES", file=file, flush=True)
     print("-"*len_header, file=file, flush=True)
 
     line_tmpl = '{:<20}|  '+'{:>20}'*6
     print(line_tmpl.format('Mixer', 'balance.P_tot', 'designed_stream', 'Fl_calc:stat:P', 'Fl_calc:stat:area', 'Fl_calc:stat:MN', 'ER'),
           file=file, flush=True)
+    print(line_tmpl.format('Units', '[lbf/inch2]', '[-]', '[lbf/inch2]', '[inch2]', '[-]', '[-]'),
+          file=file, flush=True)
+    print("-" * len_header, file=file, flush=True)
 
     line_tmpl = '{:<20}|  {:20.3f}{:^20}'+'{:20.3f}'*3
     for e_name in element_names:
@@ -247,6 +261,53 @@ def print_mixer(prob, element_names, file=sys.stdout):
                                    prob[e_name+'.Fl_I2_calc:stat:MN'][0]),
                                    prob[e_name+'.ER',][0],
                   file=file, flush=True)
+
+
+def print_gearbox(prob, element_names, file=sys.stdout):
+
+    len_header = 23+20*6
+
+    print("-"*len_header, file=file, flush=True)
+    print("                                                        GEARBOX PROPERTIES", file=file, flush=True)
+    print("-"*len_header, file=file, flush=True)
+
+    line_tmpl = '{:<20}|  '+'{:>20}'*5
+    print(line_tmpl.format('Gearbox', 'Gear ratio', 'Nin', 'Nout', 'trqin', 'trqout'), file=file)
+    print(line_tmpl.format('Units', '[-]', '[rpm]', '[rpm]', '[ft*lbf]', '[ft*lbf]'), file=file)
+    print("-" * len_header, file=file, flush=True)
+
+    line_tmpl = '{:<20}|  '+'{:20.3f}'*5
+    for e_name in element_names:
+        print(line_tmpl.format(e_name, 1/prob[e_name+'.gear_ratio'][0],
+                               prob[e_name+'.N_in'][0],
+                               prob[e_name+'.N_out'][0],
+                               prob[e_name+'.trq_in'][0],
+                               prob[e_name+'.trq_out'][0]),
+              file=file, flush=True)
+
+
+def print_heatexchanger(prob, element_names, file=sys.stdout):
+
+    len_header = 23+20*6
+
+    print("-"*len_header, file=file, flush=True)
+    print("                                                     HEAT EXCHANGER PROPERTIES", file=file, flush=True)
+    print("-"*len_header, file=file, flush=True)
+
+    line_tmpl = '{:<20}|  '+'{:>20}'*6
+    print(line_tmpl.format('Heat Exchanger', 'NTU', 'Qactual', 'htin_fluid', 'htout_fluid', 'htin_cool', 'htout_cool'), file=file)
+    print(line_tmpl.format('Units', '[-]', '[Btu/s]', '[Btu/lbm]', '[Btu/lbm]', '[Btu/lbm]', '[Btu/lbm]'), file=file)
+    print("-" * len_header, file=file, flush=True)
+
+    line_tmpl = '{:<20}|  '+'{:20.3f}'*6
+    for e_name in element_names:
+        print(line_tmpl.format(e_name, 1/prob[e_name+'.NTU'][0],
+                               prob[e_name+'.q_actual'][0],
+                               prob[e_name+'.Fl_I1:tot:h'][0],
+                               prob[e_name+'.Fl_O1:tot:h'][0],
+                               prob[e_name+'.Fl_I2:tot:h'][0],
+                               prob[e_name+'.Fl_O2:tot:h'][0]),
+              file=file, flush=True)
 
 
 def plot_compressor_maps(prob, element_names, eff_vals=np.array([0,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1.0]),alphas=[0]):
